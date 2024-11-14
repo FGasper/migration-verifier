51e38152acbe88ee589f17a6eeb27d93e7b937be
Felipe Gasper <felipe@felipegasper.com>
2024-11-14 15:00:04 -0500

    more clarity on end of task

--- internal/verifier/migration_verifier.go
+++ internal/verifier/migration_verifier.go
@@ -544,6 +544,7 @@ func (verifier *Verifier) compareOneDocument(srcClientDoc, dstClientDoc bson.Raw
 
 func (verifier *Verifier) ProcessVerifyTask(workerNum int, task *VerificationTask) {
 	verifier.logger.Debug().Msgf("[Worker %d] Processing verify task %v", workerNum, task.PrimaryKey)
+	defer verifier.logger.Debug().Msgf("[Worker %d] Done processing verify task %v", workerNum, task.PrimaryKey)
 
 	problems, docsCount, bytesCount, err := verifier.FetchAndCompareDocuments(
 		context.Background(),
