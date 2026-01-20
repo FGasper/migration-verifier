c6ad3b75baace73902b43d3cf59219cc4bbd6a47
Felipe Gasper <felipe@felipegasper.com>
2026-01-20 11:53:55 -0500

    1 task per namespace

--- internal/partitions/natural.go
+++ internal/partitions/natural.go
@@ -2,13 +2,11 @@ package partitions
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/10gen/migration-verifier/chanutil"
 	"github.com/10gen/migration-verifier/internal/logger"
 	"github.com/10gen/migration-verifier/internal/types"
 	"github.com/10gen/migration-verifier/internal/util"
-	"github.com/10gen/migration-verifier/mmongo/cursor"
 	"github.com/10gen/migration-verifier/option"
 	"github.com/mongodb-labs/migration-tools/bsontools"
 	"github.com/pkg/errors"
@@ -54,78 +52,6 @@ func PartitionCollectionNaturalOrder(
 		return pChan, nil
 	}
 
-	collSizeInBytes, docCount, _, err := GetSizeAndDocumentCount(ctx, subLogger, coll)
-	if err != nil {
-		return nil, errors.Wrapf(err, "getting collection size & count")
-	}
-
-	if docCount == 0 {
-		// Again: if the collection is empty, there’s no point in partitioning.
-		close(pChan)
-
-		return pChan, nil
-	}
-
-	idealNumPartitions := util.DivideToF64(collSizeInBytes, idealPartitionBytes)
-
-	// Use min() to prevent docCount==0 or some other silliness from
-	// causing a failure.
-	sampleRate := min(1, util.DivideToF64(idealNumPartitions, docCount))
-
-	cmd := bson.D{
-		{"find", coll.Name()},
-		{"hint", bson.D{{"$natural", 1}}},
-
-		// We fetch partition boundaries one at a time.
-		{"batchSize", 1},
-		{"filter", bson.D{{"$sampleRate", sampleRate}}},
-		{"$_requestResumeToken", true},
-		{"readConcern", bson.D{
-			{"level", "majority"},
-		}},
-
-		// Discard the actual document. All we want are the resume tokens.
-		{"projection", bson.D{
-			{"_id", 0},
-			{"_", bson.D{{"$literal", true}}},
-		}},
-	}
-
-	sess, err := coll.Database().Client().StartSession()
-	if err != nil {
-		return nil, errors.Wrapf(err, "starting session")
-	}
-
-	sessCtx := mongo.NewSessionContext(ctx, sess)
-
-	resp := coll.Database().RunCommand(sessCtx, cmd)
-
-	c, err := cursor.New(coll.Database(), resp, sess)
-	if err != nil {
-		return nil, errors.Wrapf(err, "opening partition query (%+v)", cmd)
-	}
-
-	curTokenOpt, err := cursor.GetResumeToken(c)
-	if err != nil {
-		return nil, errors.Wrapf(err, "extracting resume token")
-	}
-
-	// Confirm that we can, in fact, partition this collection naturally:
-	if curToken, hasToken := curTokenOpt.Get(); hasToken {
-		recIDRV, err := curToken.LookupErr(RecordID)
-		if err != nil {
-			return nil, errors.Wrapf(err, "extracting record ID from resume token (%v)", curToken)
-		}
-
-		switch recIDRV.Type {
-		case bson.TypeInt64, bson.TypeBinary:
-			// All good! We recognize these record ID types.
-		default:
-			// This likely indicates a new, unexpected collection type.
-			return nil, fmt.Errorf("unknown BSON type (%s) for record ID (%s)", recIDRV.Type, recIDRV)
-		}
-	}
-
 	// Partitions by record ID are only meaningful on a single mongod.
 	// (i.e., 2 nodes within the same replica set will have different
 	// record IDs for the same document)
@@ -147,89 +73,183 @@ func PartitionCollectionNaturalOrder(
 		return nil, errors.Wrapf(err, "parsing hostname in isMaster")
 	}
 
-	go func() {
-		defer close(pChan)
+	err = chanutil.WriteWithDoneCheck(
+		ctx,
+		pChan,
+		mo.Ok(Partition{
+			Natural:         true,
+			HostnameAndPort: option.Some(hostnameAndPort),
+			Key: PartitionKey{
+				Lower: bsontools.ToRawValue(bson.Null{}),
+			},
+			Upper: topRecordIDOpt.MustGet(),
+		}),
+	)
 
-		priorToken := bsontools.ToRawValue(bson.Null{})
-		var err error
+	if err != nil {
+		return nil, err
+	}
 
-		for !c.IsFinished() {
-			var curTokenOpt option.Option[bson.Raw]
-			curTokenOpt, err = cursor.GetResumeToken(c)
-			if err != nil {
-				err = errors.Wrapf(err, "reading resume token from server response")
-				break
-			}
+	/*
 
-			curToken, hasToken := curTokenOpt.Get()
-			if !hasToken {
-				break
-			}
-
-			var recIDRV bson.RawValue
-			recIDRV, err = curToken.LookupErr(RecordID)
-			if err != nil {
-				err = errors.Wrapf(err, "reading record ID from resume token (%v)", curToken)
-				break
-			}
-
-			if recIDRV.Type == bson.TypeNull {
-				recIDRV = topRecordIDOpt.MustGetf("got null resume token but have no top record ID?!?")
-			}
-
-			partition := Partition{
-				Natural:         true,
-				HostnameAndPort: option.Some(hostnameAndPort),
-				Ns:              &Namespace{coll.Database().Name(), coll.Name()},
-				Key: PartitionKey{
-					Lower: priorToken,
-				},
-				Upper: recIDRV,
-			}
-
-			err = chanutil.WriteWithDoneCheck(
-				ctx,
-				pChan,
-				mo.Ok(partition),
-			)
-
-			if err != nil && !errors.Is(err, context.Canceled) {
-				subLogger.Warn().
-					Err(err).
-					Any("partition", partition).
-					Msg("Failed to report new partition.")
-
-				return
-			}
-
-			if c.IsFinished() {
-				break
-			}
-
-			priorToken = bsontools.ToRawValue(curToken)
-
-			err = c.GetNext(ctx, bson.E{"batchSize", 1})
-			if err != nil {
-				err = errors.Wrapf(err, "fetching next partition bound")
-				break
-			}
-		}
-
+		collSizeInBytes, docCount, _, err := GetSizeAndDocumentCount(ctx, subLogger, coll)
 		if err != nil {
-			err := chanutil.WriteWithDoneCheck(
-				ctx,
-				pChan,
-				mo.Err[Partition](err),
-			)
+			return nil, errors.Wrapf(err, "getting collection size & count")
+		}
 
-			if err != nil && !errors.Is(err, context.Canceled) {
-				subLogger.Warn().
-					Err(err).
-					AnErr("cursorErr", err).
-					Msg("Failed to report cursor error.")
+		if docCount == 0 {
+			// Again: if the collection is empty, there’s no point in partitioning.
+			close(pChan)
+
+			return pChan, nil
+		}
+
+		idealNumPartitions := util.DivideToF64(collSizeInBytes, idealPartitionBytes)
+
+		// Use min() to prevent docCount==0 or some other silliness from
+		// causing a failure.
+		sampleRate := min(1, util.DivideToF64(idealNumPartitions, docCount))
+
+		cmd := bson.D{
+			{"find", coll.Name()},
+			{"hint", bson.D{{"$natural", 1}}},
+
+			// We fetch partition boundaries one at a time.
+			{"batchSize", 1},
+			{"filter", bson.D{{"$sampleRate", sampleRate}}},
+			{"$_requestResumeToken", true},
+			{"readConcern", bson.D{
+				{"level", "majority"},
+			}},
+
+			// Discard the actual document. All we want are the resume tokens.
+			{"projection", bson.D{
+				{"_id", 0},
+				{"_", bson.D{{"$literal", true}}},
+			}},
+		}
+
+		sess, err := coll.Database().Client().StartSession()
+		if err != nil {
+			return nil, errors.Wrapf(err, "starting session")
+		}
+
+		sessCtx := mongo.NewSessionContext(ctx, sess)
+
+		resp := coll.Database().RunCommand(sessCtx, cmd)
+
+		c, err := cursor.New(coll.Database(), resp, sess)
+		if err != nil {
+			return nil, errors.Wrapf(err, "opening partition query (%+v)", cmd)
+		}
+
+		curTokenOpt, err := cursor.GetResumeToken(c)
+		if err != nil {
+			return nil, errors.Wrapf(err, "extracting resume token")
+		}
+
+		// Confirm that we can, in fact, partition this collection naturally:
+		if curToken, hasToken := curTokenOpt.Get(); hasToken {
+			recIDRV, err := curToken.LookupErr(RecordID)
+			if err != nil {
+				return nil, errors.Wrapf(err, "extracting record ID from resume token (%v)", curToken)
+			}
+
+			switch recIDRV.Type {
+			case bson.TypeInt64, bson.TypeBinary:
+				// All good! We recognize these record ID types.
+			default:
+				// This likely indicates a new, unexpected collection type.
+				return nil, fmt.Errorf("unknown BSON type (%s) for record ID (%s)", recIDRV.Type, recIDRV)
 			}
 		}
-	}()
+
+
+
+		go func() {
+			defer close(pChan)
+
+			priorToken := bsontools.ToRawValue(bson.Null{})
+			var err error
+
+			for !c.IsFinished() {
+				var curTokenOpt option.Option[bson.Raw]
+				curTokenOpt, err = cursor.GetResumeToken(c)
+				if err != nil {
+					err = errors.Wrapf(err, "reading resume token from server response")
+					break
+				}
+
+				curToken, hasToken := curTokenOpt.Get()
+				if !hasToken {
+					break
+				}
+
+				var recIDRV bson.RawValue
+				recIDRV, err = curToken.LookupErr(RecordID)
+				if err != nil {
+					err = errors.Wrapf(err, "reading record ID from resume token (%v)", curToken)
+					break
+				}
+
+				if recIDRV.Type == bson.TypeNull {
+					recIDRV = topRecordIDOpt.MustGetf("got null resume token but have no top record ID?!?")
+				}
+
+				partition := Partition{
+					Natural:         true,
+					HostnameAndPort: option.Some(hostnameAndPort),
+					Ns:              &Namespace{coll.Database().Name(), coll.Name()},
+					Key: PartitionKey{
+						Lower: priorToken,
+					},
+					Upper: recIDRV,
+				}
+
+				err = chanutil.WriteWithDoneCheck(
+					ctx,
+					pChan,
+					mo.Ok(partition),
+				)
+
+				if err != nil && !errors.Is(err, context.Canceled) {
+					subLogger.Warn().
+						Err(err).
+						Any("partition", partition).
+						Msg("Failed to report new partition.")
+
+					return
+				}
+
+				if c.IsFinished() {
+					break
+				}
+
+				priorToken = bsontools.ToRawValue(curToken)
+
+				err = c.GetNext(ctx, bson.E{"batchSize", 1})
+				if err != nil {
+					err = errors.Wrapf(err, "fetching next partition bound")
+					break
+				}
+			}
+
+			if err != nil {
+				err := chanutil.WriteWithDoneCheck(
+					ctx,
+					pChan,
+					mo.Err[Partition](err),
+				)
+
+				if err != nil && !errors.Is(err, context.Canceled) {
+					subLogger.Warn().
+						Err(err).
+						AnErr("cursorErr", err).
+						Msg("Failed to report cursor error.")
+				}
+			}
+		}()
+	*/
 
 	return pChan, nil
 }
